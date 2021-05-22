59414ebe280f7c2388acbbad92b30e3bda6dc7df
multipassport <mashross@gmail.com>
2021-05-22 15:10:36 +0300

    Customize Flat Admin

--- property/admin.py
+++ property/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Flat
 
+class RealtorAdmin(admin.ModelAdmin):
+    search_fields = ('town', 'address', 'owner')
+    readonly_fields = ('created_at',)
 
-admin.site.register(Flat)
+
+admin.site.register(Flat, RealtorAdmin)
