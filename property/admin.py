e3df6a48fcc2b2374d938bffdfd490f206aa9b72
multipassport <mashross@gmail.com>
2021-05-22 18:20:02 +0300

    Add filters to admin page

--- property/admin.py
+++ property/admin.py
@@ -7,6 +7,7 @@ class RealtorAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at',)
     list_display = ('address', 'price', 'new_building', 'construction_year')
     list_editable = ('new_building',)
+    list_filter = ('new_building', 'rooms_number', 'has_balcony')
 
 
 admin.site.register(Flat, RealtorAdmin)
