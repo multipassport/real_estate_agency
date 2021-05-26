f58d2a6ccdf19ed0c0fe16cc42722e9f51e01303
multipassport <mashross@gmail.com>
2021-05-26 21:33:06 +0300

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
 
 class RealtorAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building', 'construction_year')
+    list_display = ('address', 'price', 'new_building', 'construction_year',
+        'owner_pure_phone', 'owners_phonenumber')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+    raw_id_fields = ('liked_by',)
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author', 'flat',)
 
 
 admin.site.register(Flat, RealtorAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
--- property/models.py
+++ property/models.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField('Номер владельца', max_length=20,
+        blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -46,8 +50,21 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
-
     new_building = models.NullBooleanField('Новостройка')
+
+    liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул',
+        related_name='flat_likes', blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    author = models.ForeignKey(User, related_name='complaints', on_delete=models.CASCADE,
+        verbose_name='Кто Жаловался:')
+    flat = models.ForeignKey(Flat, related_name='complaints', on_delete=models.CASCADE,
+        verbose_name='Квартира, на которую пожаловались:')
+    text = models.TextField('Текст Жалобы')
+
+
+        
